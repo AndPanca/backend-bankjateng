a629b1c26e6f3f396d1befeed76055948c94be98
Andri Panca <andripanca11@gmail.com>
2022-09-14 08:58:01 +0700

    create table card

---
+++ database/migrations/2022_09_14_014959_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->string('holder');
+            $table->date('expiry_date');
+            $table->string('security_code');
+            $table->enum('type', ['credit_card', 'silver_card'])->default('credit_card');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
