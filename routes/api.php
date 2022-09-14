72e8569e7c2f39b6a0c128962b867519493bf7bb
Andri Panca <andripanca11@gmail.com>
2022-09-14 11:51:17 +0700

    create api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// login
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // user detail
+    Route::get('/user', [App\Http\Controllers\AuthController::class, 'user']);
+    
+    // card
+    Route::get('/card', [App\Http\Controllers\CardController::class, 'index'])->name('card.index');
+    Route::post('/card', [App\Http\Controllers\CardController::class, 'create'])->name('card.create');
+    Route::get('/card/{id}', [App\Http\Controllers\CardController::class, 'show'])->name('card.show');
+    Route::delete('/card/{id}', [App\Http\Controllers\CardController::class, 'destroy'])->name('card.delete');
+    Route::put('/setpin/{id}', [App\Http\Controllers\CardController::class, 'set_pin'])->name('card.setpin');
+
+    Route::get('/transaction', [App\Http\Controllers\TransactionController::class, 'index'])->name('transaction.index');
+
+    // logout
+    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 });
+
